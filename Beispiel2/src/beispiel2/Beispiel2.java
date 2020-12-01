4dd4fd40bac5c1da13a656f2107cba7fb9e84fcd
Trinkfass Lisa <Trinkfass.Lisa@sus.htl-grieskirchen.at>
2020-12-01 08:07:18 +0100

    Beispiel 2 anfang

---
+++ Beispiel2/src/beispiel2/Beispiel2.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beispiel2;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Lisa
+ */
+public class Beispiel2 {
+
+    private final static File file = new File("numbers0");
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        int value = 0;
+        List<Integer> numbers = new ArrayList<>();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            value = Integer.parseInt(br.readLine());
+            String num = br.readLine();
+            String[] x = num.split(" ");
+            for (int i = 0; i < x.length; i++) {
+                numbers.add(Integer.parseInt(x[i]));
+            }
+        } catch (FileNotFoundException ex1) {
+            Logger.getLogger(Beispiel2.class.getName()).log(Level.SEVERE, null, ex1);
+        } catch (IOException ex1) {
+            Logger.getLogger(Beispiel2.class.getName()).log(Level.SEVERE, null, ex1);
+        }
+
+        int anzahlThreads = numbers.size() - value + 1;
+        ExecutorService ex = Executors.newFixedThreadPool(anzahlThreads);
+        List<createSubstring> tasks = new ArrayList<>();
+        for (int i = 0; i < anzahlThreads; i++) {
+            tasks.add(new createSubstring(numbers, value, i));
+        }
+        System.out.println(anzahlThreads);
+
+        List<Future<Integer>> sizes = ex.invokeAll(tasks);
+        int max = Integer.MIN_VALUE;
+        for (Future<Integer> size : sizes) {
+            if (size.get() > max) {
+                max = size.get();
+            }
+        }
+        ex.shutdown();
+
+        System.out.println("Die maximale Anzahl an unterschiedlichen Nummern ist: " + max);
+
+    }
+
+}
---
+++ Beispiel2/src/beispiel2/createSubstring.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beispiel2;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+/**
+ *
+ * @author Lisa
+ */
+public class createSubstring implements Callable<Integer> {
+
+    private List<Integer> numbers;
+    private int value;
+    private int start;
+
+    public createSubstring(List<Integer> numbers, int value, int start) {
+        this.numbers = numbers;
+        this.value = value;
+        this.start = start;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        List<Integer> sublist = numbers.subList(start, start + value);
+        Set<Integer> set = new HashSet<>();
+        set.addAll(sublist);
+        print(sublist, set.size());
+        return set.size();
+    }
+
+    private synchronized void print(List<Integer> list, int count) {
+        // System.out.println("");
+        System.out.print("<");
+        for (Integer integer : list) {
+            System.out.print(integer + ", ");
+        }
+        System.out.print("> - Hat " + count + " unterschiedlichen Zahlen");
+        System.out.println("");
+    }
+
+}
